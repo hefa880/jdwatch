65173472c9f5df36c4bb9d6265757d9695a6da46
hefaqiang <hefaqiang@cylan.com.cn>
2016-08-23 10:17:57 +0800

    添加网页浏览

---
+++ WebBrower/webbrower/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace webbrower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxAddrInput = new System.Windows.Forms.TextBox();
+            this.textBoxPriseInput = new System.Windows.Forms.TextBox();
+            this.buttonWebGo = new System.Windows.Forms.Button();
+            this.webBrowserTest = new System.Windows.Forms.WebBrowser();
+            this.buttonModifyP = new System.Windows.Forms.Button();
+            this.textBoxWareId = new System.Windows.Forms.TextBox();
+            this.buttonModify = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxAddrInput
+            // 
+            this.textBoxAddrInput.Location = new System.Drawing.Point(12, 8);
+            this.textBoxAddrInput.Name = "textBoxAddrInput";
+            this.textBoxAddrInput.Size = new System.Drawing.Size(728, 21);
+            this.textBoxAddrInput.TabIndex = 0;
+            // 
+            // textBoxPriseInput
+            // 
+            this.textBoxPriseInput.Location = new System.Drawing.Point(948, 8);
+            this.textBoxPriseInput.Name = "textBoxPriseInput";
+            this.textBoxPriseInput.Size = new System.Drawing.Size(68, 21);
+            this.textBoxPriseInput.TabIndex = 1;
+            // 
+            // buttonWebGo
+            // 
+            this.buttonWebGo.Location = new System.Drawing.Point(741, 8);
+            this.buttonWebGo.Name = "buttonWebGo";
+            this.buttonWebGo.Size = new System.Drawing.Size(75, 23);
+            this.buttonWebGo.TabIndex = 2;
+            this.buttonWebGo.Text = "转到";
+            this.buttonWebGo.UseVisualStyleBackColor = true;
+            this.buttonWebGo.Click += new System.EventHandler(this.buttonWebGo_Click);
+            // 
+            // webBrowserTest
+            // 
+            this.webBrowserTest.Location = new System.Drawing.Point(12, 39);
+            this.webBrowserTest.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserTest.Name = "webBrowserTest";
+            this.webBrowserTest.Size = new System.Drawing.Size(1154, 428);
+            this.webBrowserTest.TabIndex = 3;
+            // 
+            // buttonModifyP
+            // 
+            this.buttonModifyP.Location = new System.Drawing.Point(1021, 6);
+            this.buttonModifyP.Name = "buttonModifyP";
+            this.buttonModifyP.Size = new System.Drawing.Size(60, 23);
+            this.buttonModifyP.TabIndex = 4;
+            this.buttonModifyP.Text = "组装";
+            this.buttonModifyP.UseVisualStyleBackColor = true;
+            this.buttonModifyP.Click += new System.EventHandler(this.buttonModifyP_Click);
+            // 
+            // textBoxWareId
+            // 
+            this.textBoxWareId.Location = new System.Drawing.Point(828, 9);
+            this.textBoxWareId.Name = "textBoxWareId";
+            this.textBoxWareId.Size = new System.Drawing.Size(114, 21);
+            this.textBoxWareId.TabIndex = 5;
+            // 
+            // buttonModify
+            // 
+            this.buttonModify.Location = new System.Drawing.Point(1087, 6);
+            this.buttonModify.Name = "buttonModify";
+            this.buttonModify.Size = new System.Drawing.Size(48, 23);
+            this.buttonModify.TabIndex = 6;
+            this.buttonModify.Text = "修改";
+            this.buttonModify.UseVisualStyleBackColor = true;
+            this.buttonModify.Click += new System.EventHandler(this.buttonModify_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1165, 479);
+            this.Controls.Add(this.buttonModify);
+            this.Controls.Add(this.textBoxWareId);
+            this.Controls.Add(this.buttonModifyP);
+            this.Controls.Add(this.webBrowserTest);
+            this.Controls.Add(this.buttonWebGo);
+            this.Controls.Add(this.textBoxPriseInput);
+            this.Controls.Add(this.textBoxAddrInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxAddrInput;
+        private System.Windows.Forms.TextBox textBoxPriseInput;
+        private System.Windows.Forms.Button buttonWebGo;
+        private System.Windows.Forms.WebBrowser webBrowserTest;
+        private System.Windows.Forms.Button buttonModifyP;
+        private System.Windows.Forms.TextBox textBoxWareId;
+        private System.Windows.Forms.Button buttonModify;
+    }
+}
+
---
+++ WebBrower/webbrower/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace webbrower
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        
+            textBoxAddrInput.Text = "https://shop.jd.com/index.action";
+            textBoxWareId.Text = "10103045034";
+            webBrowserTest.Navigate(textBoxAddrInput.Text);
+            webBrowserTest.ScriptErrorsSuppressed = true;
+        }
+
+        private void buttonWebGo_Click(object sender, EventArgs e)
+        {
+            webBrowserTest.Navigate(textBoxAddrInput.Text);
+        }
+        
+        /// <summary>
+        /// 获取输入价格，并将此价格填充到指定的表中，然后提交
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonModifyP_Click(object sender, EventArgs e)
+        {
+            // 获取输入商品ID: 10103045034
+            string strWareId = textBoxWareId.Text.Trim();  //获取输入商品ID
+            string strAddr = "https://ware.shop.jd.com/ware/publish/ware_editWare.action?wid="+strWareId+"&saleStatus=onSale";
+            
+            textBoxAddrInput.Text = strAddr;
+
+            //打开指定网页
+            webBrowserTest.Navigate(strAddr);
+        }
+
+        private void GetElems()
+        {
+            string strPrice = textBoxPriseInput.Text.Trim();
+            while (webBrowserTest.ReadyState != WebBrowserReadyState.Complete) ;
+            //获取指定元素价格
+            if( strPrice == "" )
+            {
+                strPrice = "9999.00";
+            }
+
+            HtmlElement elems = webBrowserTest.Document.GetElementById("price_1547893111");
+            if (null == elems)
+            {
+                MessageBox.Show("没有找到price_1547893111");
+            }
+            else
+            {
+                elems.SetAttribute("value", strPrice);
+            }
+
+            elems = webBrowserTest.Document.GetElementById("warePubBtn");
+            if (null == elems)
+            {
+                MessageBox.Show("没有找到 warePubBtn ");
+            }
+            else
+            {
+                elems.SetAttribute("value", "保存-找到");
+                elems.InvokeMember("click");
+            }
+        }
+
+        private void buttonModify_Click(object sender, EventArgs e)
+        {
+            GetElems();
+        }
+
+        private void  WriteWebBrowserRegKey(string fileName, string strValue)
+        {
+            // HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_BROWSER_EMULATION
+            RegistryKey key = Registry.LocalMachine;
+            RegistryKey software = key.OpenSubKey("SOFTWARE\\Microsoft\\Internet Explorer\\MAIN\\FeatureControl\\FEATURE_BROWSER_EMULATION\\test", true); //该项必须已存在
+            //software.SetValue("webbrower.exe", "7000", RegistryValueKind.DWord);
+            //在HKEY_LOCAL_MACHINE\SOFTWARE\test下创建一个名为“test”，值为“博客园”的键值。如果该键值原本已经存在，则会修改替换原来的键值，如果不存在则是创建该键值。
+            // 注意：SetValue()还有第三个参数，主要是用于设置键值的类型，如：字符串，二进制，Dword等等~~默认是字符串。如：
+            software.SetValue("test", "0", RegistryValueKind.DWord); //二进制信息
+            key.Close();
+        }
+    }
+}
