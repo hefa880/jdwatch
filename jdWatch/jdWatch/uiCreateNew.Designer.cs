ffbed6c9dd6a4fb57d4f643fbe972e5713df6aa4
hefa880 <hefa880@163,com>
2016-09-26 02:54:56 +0800

    更新了数据库内容

---
+++ jdWatch/jdWatch/uiCreateNew.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace jdWatch
+{
+    partial class uiCreateNew
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewUiCreateNew = new System.Windows.Forms.DataGridView();
+            this.buttonUiCreateNewSave = new System.Windows.Forms.Button();
+            this.buttonUiCreateNewSelect = new System.Windows.Forms.Button();
+            this.Column5 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUiCreateNew)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewUiCreateNew
+            // 
+            this.dataGridViewUiCreateNew.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUiCreateNew.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column5,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column6});
+            this.dataGridViewUiCreateNew.Location = new System.Drawing.Point(12, 29);
+            this.dataGridViewUiCreateNew.Name = "dataGridViewUiCreateNew";
+            this.dataGridViewUiCreateNew.RowTemplate.Height = 23;
+            this.dataGridViewUiCreateNew.Size = new System.Drawing.Size(634, 322);
+            this.dataGridViewUiCreateNew.TabIndex = 0;
+            // 
+            // buttonUiCreateNewSave
+            // 
+            this.buttonUiCreateNewSave.Location = new System.Drawing.Point(321, 365);
+            this.buttonUiCreateNewSave.Name = "buttonUiCreateNewSave";
+            this.buttonUiCreateNewSave.Size = new System.Drawing.Size(105, 23);
+            this.buttonUiCreateNewSave.TabIndex = 1;
+            this.buttonUiCreateNewSave.Text = "保存";
+            this.buttonUiCreateNewSave.UseVisualStyleBackColor = true;
+            this.buttonUiCreateNewSave.Click += new System.EventHandler(this.buttonUiCreateNewSave_Click);
+            // 
+            // buttonUiCreateNewSelect
+            // 
+            this.buttonUiCreateNewSelect.Location = new System.Drawing.Point(90, 366);
+            this.buttonUiCreateNewSelect.Name = "buttonUiCreateNewSelect";
+            this.buttonUiCreateNewSelect.Size = new System.Drawing.Size(75, 23);
+            this.buttonUiCreateNewSelect.TabIndex = 2;
+            this.buttonUiCreateNewSelect.Text = "全选";
+            this.buttonUiCreateNewSelect.UseVisualStyleBackColor = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "选择";
+            this.Column5.Name = "Column5";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "牌品";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "系列";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "颜色";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "版本";
+            this.Column4.Name = "Column4";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "备注";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // uiCreateNew
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 394);
+            this.Controls.Add(this.buttonUiCreateNewSelect);
+            this.Controls.Add(this.buttonUiCreateNewSave);
+            this.Controls.Add(this.dataGridViewUiCreateNew);
+            this.Name = "uiCreateNew";
+            this.Text = "uiCreateNew";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUiCreateNew)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewUiCreateNew;
+        private System.Windows.Forms.Button buttonUiCreateNewSave;
+        private System.Windows.Forms.Button buttonUiCreateNewSelect;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
---
+++ jdWatch/jdWatch/uiCreateNew.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Fatq.SqlCommit.Mode;
+using UiInputDataStruct.Mode;
+
+namespace jdWatch
+{
+    public partial class uiCreateNew : Form
+    {
+        IList<InputDataStruct> uiCreateNew_list = new List<InputDataStruct>();
+        public uiCreateNew()
+        {
+            InitializeComponent();
+        }
+
+        private void uiCreateNew_GetDataGridViewData()
+        {
+            uiCreateNew_list.Clear();
+            int i = -1;
+            foreach (DataGridViewRow dr in dataGridViewUiCreateNew.Rows)
+            {
+                i++;
+                if (null == dr.Cells[0].FormattedValue)
+                {
+                    continue;
+                }
+
+                if (dr.Cells[0].FormattedValue.ToString() == true.ToString() && null != dr.Cells[2].Value)
+                {
+                    InputDataStruct inputData = new InputDataStruct();
+
+                    inputData.data = new CUiInputDataStruct();
+                    if (null != dr.Cells[1].Value)
+                        inputData.data.ProductSeller = dr.Cells[1].Value.ToString().Trim();
+                    if (null != dr.Cells[2].Value)
+                        inputData.data.ProductSkuid = dr.Cells[2].Value.ToString().Trim();
+
+                    if (null != dr.Cells[3].Value)
+                        inputData.data.ProductName = dr.Cells[3].Value.ToString().Trim();
+                    if (null != dr.Cells[4].Value)
+                        inputData.data.ProductSerial = dr.Cells[4].Value.ToString().Trim();
+                    if (null != dr.Cells[5].Value)
+                        inputData.data.ProductColor = dr.Cells[5].Value.ToString().Trim();
+                    if (null != dr.Cells[6].Value)
+                        inputData.data.ProductVersion = dr.Cells[6].Value.ToString().Trim();
+
+                    if (null != dr.Cells[8].Value)
+                        inputData.data.ProductWarnPrice = Convert.ToDouble(dr.Cells[8].Value.ToString().Trim());
+                    if (null != dr.Cells[9].Value)
+                        inputData.data.ProductWarnDriect = dr.Cells[9].Value.ToString().Trim();
+                    if (null != dr.Cells[10].Value)
+                        inputData.data.ProductState = dr.Cells[10].Value.ToString().Trim();
+                    inputData.data.index = i;
+
+                    uiCreateNew_list.Add(inputData);
+                }
+            }
+        }
+
+        private void UiCreateNew_ShowSaveStatus()
+        {
+            for (int i = 0, j = 0; i < dataGridViewUiCreateNew.Rows.Count - 1 && j < uiCreateNew_list.Count; i++)
+            {
+                if (dataGridViewUiCreateNew.Rows[i].Cells[6].Value.ToString().Trim() == uiCreateNew_list[j].data.ProductSkuid)
+                {
+                    if (uiCreateNew_list[j].data.ProductState == "新增")
+                    {
+                        dataGridViewUiCreateNew.Rows.RemoveAt(i);
+                        uiCreateNew_list.Remove(uiCreateNew_list[j]);
+                        i--;
+                    }
+                    else
+                    {
+                        dataGridViewUiCreateNew.Rows[i].Cells[10].Value = uiCreateNew_list[j].data.ProductState;
+                        j++;
+                    }
+                }
+            }
+            //  uiInput_list.Clear();
+        }
+
+        private void buttonUiCreateNewSave_Click(object sender, EventArgs e)
+        {
+            bool bRet = false;
+            int countSuccessed = 0;
+            int countFailed = 0;
+            SqlCommit sqlconn = new SqlCommit();
+
+            uiCreateNew_GetDataGridViewData();
+            if (0 == uiCreateNew_list.Count)
+            {
+                return;
+            }
+            // 保存到数据库
+            for (int i = 0; i < uiCreateNew_list.Count; i++)
+            {
+                bRet = sqlconn.Sqlcommit_Insert(uiCreateNew_list[i].data);
+                if (true == bRet)
+                {
+                    uiCreateNew_list[i].data.ProductState = "新增";
+                    countSuccessed++;
+                }
+                else
+                {
+                    uiCreateNew_list[i].data.ProductState = "已有";
+                    countFailed++;
+                }
+            }
+
+            //检查链表数据，如果提交成功则从dataGridViewUiIpnut中删除，否则保留，并在状态列显示原因
+            UiCreateNew_ShowSaveStatus();
+            if (uiCreateNew_list.Count > 0) /// && this.Text != "编辑数据库")
+            {
+                DialogResult dRet = DialogResult.No;
+
+                dRet = MessageBox.Show("有重复的数据，是否要覆盖？\n是，覆盖\n否，抛弃这些重复数据", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dRet == DialogResult.Yes)
+                {
+                    for (int i = 0; i < uiCreateNew_list.Count; i++)
+                    {
+                        bRet = sqlconn.Sqlcommit_Update(uiCreateNew_list[i].data);
+                    }
+                }
+
+                for (int i = 0; i < dataGridViewUiCreateNew.Rows.Count - 1; i++)
+                {
+                    if (null == dataGridViewUiCreateNew.Rows[i].Cells[0].Value)
+                    {
+                        continue;
+                    }
+
+                    if (dataGridViewUiCreateNew.Rows[i].Cells[0].Value.ToString() == true.ToString())
+                    {
+                        dataGridViewUiCreateNew.Rows.RemoveAt(i);
+                        i--; //关键在这里
+                    }
+                }
+                uiCreateNew_list.Clear();
+            }
+        }
+    }
+}
